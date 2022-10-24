95f237ecaf5f923f214e68021ec7d5977c0acf8a
hmartiins <henrique.martins@mktsystems.com>
2022-10-24 09:09:54 -0300

    changing the home page to SSG and configuring product prices

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,9 @@
 import Stripe from "stripe";
 
+if (!process.env.SECRET_KEY_STRIPE) {
+  throw new Error("process.env.SECRET_KEY_STRIPE is not set");
+}
+
 export const stripe = new Stripe(process.env.SECRET_KEY_STRIPE, {
   apiVersion: "2022-08-01",
   appInfo: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import Image from "next/future/image";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import { HomeContainer, Product } from "../styles/pages/home";
-import { stripe } from "./lib/stripe";
+import { stripe } from "../lib/stripe";
 
 import { useKeenSlider } from "keen-slider/react";
 
 import "keen-slider/keen-slider.min.css";
 import Stripe from "stripe";
-
 interface HomeProps {
   products: {
     id: string;
@@ -39,7 +38,7 @@ export default function Home({ products }: HomeProps) {
 
           <footer>
             <strong>{product.name}</strong>
-            <span>R$ {product.price}</span>
+            <span>{product.price}</span>
           </footer>
         </Product>
       ))}
@@ -47,7 +46,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -59,7 +58,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(price.unit_amount! / 100),
     };
   });
 
@@ -67,5 +69,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+
+    revalidate: 60 * 60 * 2, // 2 hours
   };
 };
